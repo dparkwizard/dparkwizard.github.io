e04d0366520eb81e1283941fa8682c75fa1c9157
Dan Park <dparkwizard@magicpoint.ai>
2024-10-26 22:02:56 -0700

    asdf

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,21 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const stripe = require('stripe')('your-secsk_test_51QEMwJQuGWNDNoDweLXBpHpsOclwBeI548PhN6aS4tfJgdD2m1SVoZXTeMFGlcsGmCBhOetn79rJGf7cQ1WQEPxt0085YNpvRH'); // Replace with your Stripe secret key
-const cors = require('cors');
+const stripe = require('./stripe');
 
 const app = express();
-app.use(cors({ origin: true }));
-app.use(express.json());
 
-app.post('/create-checkout-session', async (req, res) => {
-    const { items } = req.body;
+app.post('/checkout', async (req, res) => {
+  const { items } = req.body;
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: items,
-        mode: 'payment',
-        success_url: 'https://magicpoint.ai/success', // Replace with your success URL
-        cancel_url: 'https://magicpoint.ai/cancel',   // Replace with your cancel URL
-    });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000, // Amount in cents
+    currency: 'usd',
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
 
-    res.json({ id: session.id });
+  res.json({ clientSecret: paymentIntent.client_secret });
 });
 
 exports.api = functions.https.onRequest(app);
