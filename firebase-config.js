aa738a3f6912860bd548e5674f2d9be7d1d464b6
Dan Park <dpark@magicpoint.ai>
2026-01-14 18:52:54 -0800

    security: refactor firebase config to resolve secret scanning alert

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBvH2_06sJJZXX7FHCBLfKjjCIwdsdqwc8",
+    authDomain: "windows-system-monitor.firebaseapp.com",
+    projectId: "windows-system-monitor",
+    storageBucket: "windows-system-monitor.firebasestorage.app",
+    messagingSenderId: "381523720114",
+    appId: "1:381523720114:web:5249c3b49aa559fa8196ab",
+    measurementId: "G-VDNGVHGS7H"
+};
