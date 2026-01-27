10de84ceb26e48ca390e4890c0208c618ae4d047
Dan Park <dpark.sports@gmail.com>
2026-01-26 23:55:05 -0800

    Update Firebase config with new measurement ID and API secret

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,7 @@ export const firebaseConfig = {
     apiKey: "AIzaSyBvH2_06sJJZXX7FHCBLfKjjCIwdsdqwc8",
     authDomain: "windows-system-monitor.firebaseapp.com",
     projectId: "windows-system-monitor",
-    storageBucket: "windows-system-monitor.firebasestorage.app",
-    messagingSenderId: "381523720114",
     appId: "1:381523720114:web:5249c3b49aa559fa8196ab",
-    measurementId: "G-VDNGVHGS7H"
+    measurementId: "G-9T0L34LJZQ",
+    apiSecret: "gO2H2VNNT9mVCDYXaFd8yw"
 };
